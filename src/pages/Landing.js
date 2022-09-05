5512bcbcf3616e0ed7f1a257cf1d7ee268fec40e
8areika <8@afrika.dev>
2022-09-05 21:33:16 +0300

    feat: mobile version for landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,47 +7,100 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button";
 
 function Landing() {
-  return (
-    <>
-      <div className="landing-main">
-        <Logo />
-        <div className="column-wrapper">
-          <div className="left-column">
-            <p className="emoji">Peek a boo! 👀</p>
-            <p className="emoji-texts">Need a CV for for your dream job?</p>
-            <p className="emoji-texts">You don’t know how to make one?</p>
-            <p className="emoji-texts">but you still need to be perfect.</p>
-            <br />
-            <p className="emoji-texts">It is all good we have got your back!</p>
+  if (window.matchMedia("(max-width: 768px)")) {
+    return (
+      <>
+        <div className="landing-main">
+          <Logo />
+          <div className="column-wrapper">
+            <div className="left-column">
+              <img className="cv_paper" src={cv_paper} alt="a cv of a woman" />
 
-            <img className="cv_paper" src={cv_paper} alt="a cv of a woman" />
-          </div>
-          <div className="center-column">
-            <Link to={"userinput"}>
-              <Button label="Generate"/>
-            </Link>
-          </div>
-          <div className="right-column">
-            <img
-              className="mission_impossble"
-              src={mission_impossible}
-              alt="a man stealing data from a computer"
-            />
-            <p className="mission-impossble-texts">
-              Just give us all the necessary information
-            </p>
-            <p className="mission-impossble-texts">
-              and we will give you a perfect CV.
-            </p>
-            <br className="mission-impossble-texts" />
-            <p className="mission-impossble-texts">
-              Best part?.... you can make it in your favorite color!!
-            </p>
+              <p className="emoji">Peek a boo! 👀</p>
+              <p className="emoji-texts">Need a CV for for your dream job?</p>
+              <p className="emoji-texts">You don’t know how to make one?</p>
+              <p className="emoji-texts">but you still need to be perfect.</p>
+              <br />
+              <p className="emoji-texts">
+                It is all good we have got your back!
+              </p>
+            </div>
+            <div className="right-column">
+              <img
+                className="mission_impossble"
+                src={mission_impossible}
+                alt="a man stealing data from a computer"
+              />
+              <p className="mission-impossble-texts">
+                Just give us all the necessary
+              </p>
+              <p className="mission-impossble-texts">
+                information and we will give
+              </p>
+              <p className="mission-impossble-texts">you a perfect CV.</p>
+              <br />
+              <p className="mission-impossble-texts">Best part?....</p>
+              <p className="mission-impossble-texts">
+                you can make it in your favorite 
+              </p>
+              <p className="mission-impossble-texts">
+                color!!
+              </p>
+            </div>
+            <div className="center-column">
+              <Link to={"userinput"}>
+                <Button label="Generate" />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="landing-main">
+          <Logo />
+          <div className="column-wrapper">
+            <div className="left-column">
+              <p className="emoji">Peek a boo! 👀</p>
+              <p className="emoji-texts">Need a CV for for your dream job?</p>
+              <p className="emoji-texts">You don’t know how to make one?</p>
+              <p className="emoji-texts">but you still need to be perfect.</p>
+              <br />
+              <p className="emoji-texts">
+                It is all good we have got your back!
+              </p>
+
+              <img className="cv_paper" src={cv_paper} alt="a cv of a woman" />
+            </div>
+            <div className="center-column">
+              <Link to={"userinput"}>
+                <Button label="Generate" />
+              </Link>
+            </div>
+            <div className="right-column">
+              <img
+                className="mission_impossble"
+                src={mission_impossible}
+                alt="a man stealing data from a computer"
+              />
+              <p className="mission-impossble-texts">
+                Just give us all the necessary information
+              </p>
+              <p className="mission-impossble-texts">
+                and we will give you a perfect CV.
+              </p>
+              <br className="mission-impossble-texts" />
+              <p className="mission-impossble-texts">
+                Best part?.... you can make it in your favorite color!!
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Landing;
